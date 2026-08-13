c8cec6ee87aaa1f6a99c732893db08436e0034b5
Tanya Gupta <tanya441g@gmail.com>
2026-08-13 05:37:46 +0530

    Update useGetCurrentUser.jsx

--- frontend/hooks/useGetCurrentUser.jsx
+++ frontend/hooks/useGetCurrentUser.jsx
@@ -2,18 +2,16 @@ import { useEffect } from "react";
 import axios from "axios";
 import { useDispatch } from "react-redux";
 import { setUserData } from "../src/redux/userSlice.js";
+import { serverUrl } from "../App";
 
 const useGetCurrentUser = () => {
     const dispatch= useDispatch()
     useEffect(() => {
         const fetchUser = async () => {
             try {
-                const res = await axios.get(
-                    "http://localhost:8000/api/user/current",
-                    {
-                        withCredentials: true,
-                    }
-                );
+                const res = await axios.get(`${serverUrl}/api/user/current`, {
+    withCredentials: true
+});
                 dispatch(setUserData(res.data))
             } catch (error) {
                 console.log(error.response?.data?.message);
